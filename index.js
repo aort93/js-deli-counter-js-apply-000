6d4c10f4b3335298aadae02d515109cc354f5793
Alonso Ortega <alonsojortega@gmail.com>
2019-01-14 15:58:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function nowServing(line) {
 
 function currentLine(line) {
   if(line.length === 0) {
-    return 'The line is currently empty';
+    return 'The line is currently empty.';
   }
 }
