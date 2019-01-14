ac7b310394b40f07944e27382b3749dd94730bf0
Alonso Ortega <alonsojortega@gmail.com>
2019-01-14 15:51:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 //katzDeliLine = [];
 
+function takeANumber(line, name) {
+  //takes in a line array and a name to input into the end of the array
+  line.push(name);
+
+  return "Welcome, " + name + '. You are number ' + line.length + ' in line.'
+}
